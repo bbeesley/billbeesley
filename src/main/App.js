8dbe6e35c15a1f6555ad7f9a39edf9769e48a43e
Bill Beesley <bbeesley@me.com>
2018-04-08 23:49:02 +0200

    Creating some boilerplate

---
+++ src/main/App.js
@@ -0,0 +1,19 @@
+// @flow
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Hello World</h1>
+        </header>
+        <p>
+          This is a work in process, give me a little time...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
