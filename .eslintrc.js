aff457571ad5743fde824fda14ab9ca43cfbb109
Bill Beesley <bbeesley@me.com>
2018-04-09 13:54:42 +0000

    setup babel and webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,136 @@
 module.exports = {
-  "env": {
-    "browser": true,
+  parser: 'babel-eslint',
+  'extends': 'airbnb',
+  env: {
+    es6: true,
+    node: true,
   },
-  "parser": "babel-eslint",
-  "extends": "airbnb",
-  "rules": {
-    "react/jsx-filename-extension": 0
-  }
+  plugins: [
+    'flowtype',
+    'import',
+  ],
+  rules: {
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+    'generator-star-spacing': [
+      0,
+    ],
+    'no-duplicate-imports': [
+      0,
+    ],
+    'no-await-in-loop': [
+      0,
+    ],
+    'no-confusing-arrow': [
+      'error',
+      {
+        allowParens: true,
+      },
+    ],
+    'arrow-parens': [
+      'error',
+      'always',
+    ],
+    complexity: [
+      2,
+      6,
+    ],
+    indent: [
+      2,
+      4,
+    ],
+    'no-console': [
+      2,
+    ],
+    'max-len': [
+      2,
+      120,
+    ],
+    'spaced-comment': [
+      0,
+    ],
+    'import/prefer-default-export': [
+      0,
+    ],
+    'import/extensions': [
+      'error',
+      {
+        js: 'never',
+        json: 'always',
+      },
+    ],
+    'valid-jsdoc': 2,
+    'new-cap': [
+      2,
+      {
+        capIsNewExceptions: [
+          'Map',
+          'List',
+          'Record',
+        ],
+      },
+    ],
+    'no-underscore-dangle': [
+      'off',
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: [
+          '.js',
+        ],
+      },
+    ],
+    'react/jsx-indent': [
+      2,
+      4,
+    ],
+    'react/jsx-indent-props': [
+      2,
+      4,
+    ],
+    'react/prop-types': [
+      0,
+    ],
+    'react/no-array-index-key': [
+      'warning',
+    ],
+    'flowtype/no-dupe-keys': 2,
+    'flowtype/no-weak-types': [
+      1,
+      {
+        any: true,
+        Object: false,
+        Function: false,
+      },
+    ],
+    'flowtype/define-flow-type': 1,
+    'flowtype/require-parameter-type': 1,
+    'flowtype/type-id-match': [
+      1,
+      '^([A-Z][A-Za-z0-9]+)+Type$',
+    ],
+    'flowtype/use-flow-type': 1,
+  },
+  globals: {
+    fetch: false,
+  },
+  settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true,
+    },
+  },
 };
---
+++ webpack.config.dist.js
@@ -0,0 +1,136 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const resolve = require('path').resolve;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const StatsWriterPlugin = require("webpack-stats-plugin").StatsWriterPlugin;
+const prefixwrap = require('postcss-prefixwrap');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src/main',
+    ],
+    output: {
+        path: resolve(__dirname, 'dist', 'www'),
+        filename: `[chunkhash]-bundle.js`,
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist'], {
+            root: __dirname,
+        }),
+        new CopyWebpackPlugin([{
+            from: resolve(__dirname, 'index.html'),
+            to: resolve(__dirname, 'dist', 'index.html'),
+        }]),
+        new UglifyJSPlugin({
+            uglifyOptions: {
+                sourceMap: true,
+                mangle: true,
+                ie8: false,
+                compress: {
+                    warnings: true,
+                    conditionals: true,
+                    unused: true,
+                    comparisons: true,
+                    sequences: true,
+                    dead_code: true,
+                    evaluate: true,
+                    if_return: true,
+                    join_vars: true,
+                },
+                output: {
+                    comments: false,
+                },
+            },
+        }),
+        new ExtractTextPlugin('[contenthash]-styles.css'),
+        new StatsWriterPlugin({
+            filename: 'stats.json',
+            // transform: (stats) => JSON.stringify({
+            //     bundles: {
+            //         js: stats.assetsByChunkName.main.find(file => /\.js$/.test(file)),
+            //         css: stats.assetsByChunkName.main.find(file => /\.css$/.test(file)),
+            //     },
+            // }),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: resolve(__dirname),
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.*css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader' },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                plugins: [
+                                    autoprefixer,
+                                ]
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                        },
+                    ],
+                }),
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-sprite-loader',
+                        options: {
+                            extract: true,
+                        },
+                    },
+                    {
+                        loader: 'svgo-loader',
+                        options: {
+                            plugins: [
+                                { removeTitle: true },
+                                { convertColors: { shorthex: false } },
+                                { convertPathData: false },
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.icons\.(js|json)$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader!postcss-loader!sass-loader!webfonts-loader',
+                }),
+            },
+            {
+                test: /\.(png|jpg|eot|woff|woff2|ttf)$/,
+                loader: 'file-loader?',
+            },
+            {
+                test: /\.(txt)$/,
+                loader: 'raw-loader',
+            },
+            {
+                test: /\.ico$/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
+        ],
+    },
+    externals: {
+        fs: '{}',
+        'https-proxy-agent': '{}',
+        module: '{}',
+    },
+};
